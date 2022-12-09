685ab279ef4fc2aa02fab2bd3c9cee3d24b0285d
Euphocc <xu2954661568@gmail.com>
2022-12-09 19:45:03 +0800

    change the responsibility

--- js/commonmap/CreateCommon2.js
+++ js/commonmap/CreateCommon2.js
@@ -24,8 +24,8 @@ export class CreateCommon2 {
     this.setup(); // 开始游戏
   }
   setup = () => {
-    // window.innerHeight = 960;
-    // window.innerWidth = 1920;
+    window.innerHeight = 960;
+    window.innerWidth = 1920;
     this.app = new Application({
       width: window.innerWidth,
       height: window.innerHeight,
